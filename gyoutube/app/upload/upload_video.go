fb6de5d736f54c1016ef9db073f60622b7ebf0ab
endbot <hendri41234@gmail.com>
2025-06-06 02:16:48 +0700

    update gctts

---
+++ gyoutube/app/upload/upload_video.go
@@ -0,0 +1,24 @@
+package upload
+
+var a = `
+kalo lo perhatiin, indonesia sebenerny udah jadi tempat sampah.
+emang nya dimana lagi ada tempat bersih? di semua tempat udah bau busuk, bahkan bau nya kaya tai.
+
+kutu kutu di negara ini ngeluarin bau mulut buat negedoktrin NPC,
+seolah olah indonesia adalah emas.
+iya emang emas, tapi emas nya lembek. ngga ada yang mau nyentuh, soalnya bau bikin mual
+
+kapan yah indonesia itu hilang. gua nungguin momen itu.
+
+
+kalo liat sejarah, ada lho yang nasib nya kaya negara ini.
+contohnya sejarah 'la Revulution'.
+ Maximilien Robespiere, dia aktor utama dalam cerita nyata.
+terutama pada masa terror. dia punya julukan "The Incorruptile".
+karena idealisme nya dalam negakkin keadilan sosial.
+dia percaya kalo terror adalah cara terbaik buat negakkin kebijakkan di masyarakat baru.
+dia mimpin komite keamanan publik buat nerapin hukuman mati.
+
+ngomong ngomong, lu pernah berpikir kaya gua ngga si?
+`
+  
